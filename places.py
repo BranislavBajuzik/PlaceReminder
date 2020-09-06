8280d557ec21b74d6e1efd4ed0c7c3b7be69c766
BranislavBajuzik <branislav.bajuzik@gmail.com>
2020-09-06 09:15:29 +0200

    Added CI

--- places.py
+++ places.py
@@ -1,3 +1,5 @@
+"""Main file."""
+
 import argparse
 import string
 from pathlib import Path
@@ -6,8 +8,16 @@ from typing import Literal, Set
 
 
 def take_action(
-    file: Path, action: Literal["add", "remove", "list", "pick"], places: Set[str], place: str = None
+    path: Path, action: Literal["add", "remove", "list", "pick"], places: Set[str], place: str = None
 ) -> int:
+    """Handle user choice and data.
+
+    :param path: Path to store data in.
+    :param action: Action user requested.
+    :param places: All data in :ref:`path`.
+    :param place: Data user entered.
+    :return: Exit code.
+    """
     if action == "list":
         if places:
             print(f"Stored places ({len(places)}):")
@@ -24,10 +34,12 @@ def take_action(
         chosen = choice(tuple(places))
         print(f"'{chosen}' was chosen")
 
-        if input("Satisfied? ")[:1].lower() == "y":
-            return take_action(file, "remove", places, chosen)
+        if (satisfaction := input("Satisfied? (y/n/q): ")[:1].lower()) == "y":
+            return take_action(path, "remove", places, chosen)
+        elif satisfaction == "q":
+            return 0
         else:
-            return take_action(file, "pick", places)
+            return take_action(path, "pick", places)
 
     if place is None:
         print(f"Place required for action {action}")
@@ -49,26 +61,40 @@ def take_action(
         places.discard(place)
 
     if places:
-        file.write_text("\n".join(places), encoding="utf-8")
+        path.write_text("\n".join(places), encoding="utf-8")
     else:
-        file.unlink(missing_ok=True)
+        path.unlink(missing_ok=True)
 
     return 0
 
 
 def normalize(text: str) -> str:
+    """Normalize :ref:`text` for string operations.
+
+    :param text: String to normalize.
+    :return: Normalized string.
+    """
     return string.capwords(text.strip().casefold())
 
 
-def load_places(file: Path) -> Set[str]:
-    if not file.is_file():
+def load_places(path: Path) -> Set[str]:
+    """Deserialize data.
+
+    :param path: Path to load from.
+    :return: Deserialized data.
+    """
+    if not path.is_file():
         return set()
 
-    with file.open(encoding="utf-8") as f:
+    with path.open(encoding="utf-8") as f:
         return {stripped for line in f if (stripped := normalize(line))}
 
 
 def parse_cli() -> argparse.Namespace:
+    """Parse the command line arguments.
+
+    :return: Object with arguments bound as attributes.
+    """
     parser = argparse.ArgumentParser(prog="PlaceReminder", allow_abbrev=False)
 
     parser.add_argument("file", type=Path, help="File to read/store the places in")
@@ -81,6 +107,10 @@ def parse_cli() -> argparse.Namespace:
 
 
 def main() -> int:
+    """Program entrypoint.
+
+    :return: Exit code.
+    """
     args = parse_cli()
 
     places = load_places(args.file)
